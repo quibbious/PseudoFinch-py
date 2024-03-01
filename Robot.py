1ce2b6d4c763823ad656c7a144891734a6560b44
Quarksay <108188557+Quarksay@users.noreply.github.com>
2024-02-29 19:10:51 -0600

    Update Robot.py
    unified all functions under the Robot() class, and fixed spaghetti checks of if a number is within a range.

--- Robot.py
+++ Robot.py
@@ -1,88 +1,112 @@
 import sys
 
-class Errors():
+class Errors:
     """Contains many errors relating to the FakeFinch Robots"""
-    def __init(self, message):
-        self.message = message
-        
-    def print_error(message):
+    
+    @staticmethod
+    def printError(message):
         print("\033[91m" + f"{message}" + "\033[0m")  # ANSI escape code for red text
         sys.exit()
 
+    @staticmethod
     def RGBValueError():
-        Errors.print_error("ValueError: invalid RGB range, must be between 1-255")
+        Errors.printError("ValueError: invalid RGB range, must be between 1-255")
 
+    @staticmethod
     def checkIfInvalidRGBValue(R, G, B):
+        
+        if not R in range(0,255):
+            Errors.RGBValueError()
+        if not G in range(0,255):
+            Errors.RGBValueError()
+        if not B in range(0,255):
+            Errors.RGBValueError()
 
-        if R < 0 or R > 255:
-            Errors.RGBValueError()
-        if G < 0 or G > 255: 
-            Errors.RGBValueError()
-        if B < 0 or B > 255:
-            Errors.RGBValueError()
-
+    @staticmethod
     def directionError():
-        Errors.print_error("Invalid direction; must be 'F' or 'B'.")
+        Errors.printError("Invalid direction; must be 'F' or 'B'.")
         
     
+    @staticmethod
     def MissingRobotError():
-        Errors.print_error("No robot detected; please specify a robot.")
+        Errors.printError("No robot detected; please specify a robot.")
     
+    @staticmethod
     def SpeedValueError():
-        Errors.print_error("ValueError: Speed must be between 1-100")
-    
-class Robot(Errors):
+        Errors.printError("ValueError: Speed must be between 1-100")
+
+class Robot:
     """Constructor that creates an object corresponding to a Robot"""
-    
+    i = 1
     def __init__(robot, name):
         robot.name = name 
-
-        
-    def getVals(robot): 
-        return robot.name
-
-class RobotLights(Robot):
-    """Sets a tri-color LED in the Finch beak to a given color by setting the intensities of the red, green, and blue elements inside it.
-    The method requires three intensity values from 0-255. Setting all three intensity values to 0 turns the beak off."""
-    def __init__(robot, name):
-        super().__init__(name)
     
-    def BeakLight(R, G,B):
+    """Sets a tri-color LED in the Robot beak to a given color by setting the intensities of the red, green, and blue elements inside it.
+    The method requires three intensity values from 0-255. Setting all three intensity values to 0 turns the beak off."""
+    @staticmethod
+    def BeakLight(R, G, B):
+        
         Errors.checkIfInvalidRGBValue(R,G,B)
         
         BeakLight = [R,G,B]
-        return BeakLight
-
-class RobotController(Robot, Errors):
-    def __init__(self, robot, direction, distance, speed):
-        self.robot = robot
-        self.direction = direction
-        self.distance = distance
-        self.speed = speed
         
+        return BeakLight
+    @staticmethod
     def setMove(direction=str, distance=int, speed=int):
+        
         """Moves the robot forward 'F', or backwards 'B', followed by a distance in cm and a speed from 1-100"""
         
-        if direction != 'F' and 'B':
-            Errors.print_error(f"SyntaxError: invalid movement direction {direction}")
+        if direction != 'F' and direction != 'B':
+            
+            Errors.printError(f"SyntaxError: invalid movement direction {direction}")
             
         if speed < 0 or speed > 100:
+            
             Errors.SpeedValueError()
         
         if distance < 1: 
-            Errors.print_error("ValueError: distance cannot be below one (1)")
+            
+            Errors.printError("ValueError: distance cannot be below one (1)")
+            
+        #info for pygame to use 
         
-        return direction, distance, speed, 
-    
-    
-    
+        return direction, distance, speed
+    @staticmethod
+    def setTurn(direction=str, angle=int, speed=int):
+        if direction != 'R' and 'L': 
+            
+            Errors.printError(f"SyntaxError: invalid direction '{direction}'")
+            
+        if speed < 0 or speed > 100:
+            
+            Errors.SpeedValueError()
+            
+        if angle < 1: 
+            
+            Errors.printError("Angle must be at least 1")
+            
+        return direction, angle, speed
+    @staticmethod
+    def setMotors(L=int,R=int):
+        
+        if not L in range(0,255): 
+            Errors.printError("Speed must be between -100 and 100")
+    @staticmethod    
+    def getVals(robot):
+        """returns name of specified robot"""
+        id = 1  
+        return robot
+
+
+
+
+
 robot = Robot("Finch")
 
-RobotLights.BeakLight(0,0,0)
+robot.setMove('F',100,50)
 
-print(robot.name)
+getvals = Robot.getVals(robot.name)
 
-movement = RobotController.setMove('F', 1 , 200)
+beaklight = robot.BeakLight(100,200,100)
 
-print(movement)
-
+print(beaklight)
