89f03fae7b7ad7b6adc7a1b1d53f975d5f13575a
Quarksay <108188557+Quarksay@users.noreply.github.com>
2024-02-29 20:29:44 -0600

    Update Robot.py
    added a few functions

--- Robot.py
+++ Robot.py
@@ -36,16 +36,19 @@ class Errors:
         Errors.printError("ValueError: Speed must be between 1-100")
 
 class Robot:
+    
+    
+    
     """Constructor that creates an object corresponding to a Robot"""
     i = 1
     def __init__(robot, name):
         robot.name = name 
     
-    """Sets a tri-color LED in the Robot beak to a given color by setting the intensities of the red, green, and blue elements inside it.
-    The method requires three intensity values from 0-255. Setting all three intensity values to 0 turns the beak off."""
     @staticmethod
     def BeakLight(R, G, B):
-        
+        """Sets a tri-color LED in the Robot beak to a given color by setting the intensities of the red, green, and blue elements inside it.
+    The method requires three intensity values from 0-255. Setting all three intensity values to 0 turns the beak off.
+        Example: Robot.setBeak(0,100,0)"""
         Errors.checkIfInvalidRGBValue(R,G,B)
         
         BeakLight = [R,G,B]
@@ -54,7 +57,8 @@ class Robot:
     @staticmethod
     def setMove(direction=str, distance=int, speed=int):
         
-        """Moves the robot forward 'F', or backwards 'B', followed by a distance in cm and a speed from 1-100"""
+        """Moves the robot forward 'F', or backwards 'B', followed by a distance in cm and a speed from 1-100.
+        Example: Robot.setMove(‘F’,10,50)"""
         
         if direction != 'F' and direction != 'B':
             
@@ -73,9 +77,10 @@ class Robot:
         return direction, distance, speed
     @staticmethod
     def setTurn(direction=str, angle=int, speed=int):
-        """Turns the Finch right or left for a specified angle at a specified speed.
-        The method requires a direction (‘R’ for right or ‘L’ for left), an angle in degrees, and a speed from 0-100."""
-        if direction != 'R' and 'L': 
+        """Turns the Robot right or left for a specified angle at a specified speed.
+        The method requires a direction (‘R’ for right or ‘L’ for left), an angle in degrees, and a speed from 0-100.
+        Example: Robot.setTurn(‘R’,90,50)"""
+        if direction != 'R' and direction != 'L': 
             
             Errors.printError(f"SyntaxError: invalid direction '{direction}'")
             
@@ -86,14 +91,65 @@ class Robot:
         if not angle in range(1,361): # 360 degree limit per command 
             
             Errors.printError("ValueError: Angle out of range; must be 1-360")
-            
+
+        if direction == 'L':
+            invertedangle = -1  * angle
+            return direction, invertedangle, speed
+        
         return direction, angle, speed
     @staticmethod
     def setMotors(L=int,R=int):
-        
-        if not L in range(0,255): 
+        """Sets the Robot wheels to spin at the given speeds. The method requires two speeds between -100 and 100 for the left and right wheels.
+        Setting the speed to 0 turns the motor off.
+        Example: Robot.setMotors(-50,50)
+        """
+           
+        if not L in range(-101,101): 
             Errors.printError("Speed must be between -100 and 100")
-    @staticmethod    
+        if not R in range(-101,101):
+            Errors.printError("Speed must be between -100 and 100")
+        Motors = [L,R]
+        return Motors
+    @staticmethod
+    def stop():
+        Robot.setMotors(0,0)
+    
+    @staticmethod
+    
+    def setTail(port, r, g, b):
+        """Sets a tri-color LED in the Robot tail to a given color by setting the intensities of the red, green, and blue elements inside it.
+        The method requires the port number of the LED (1, 2, 3, 4, or “all”) and three intensity values from 0-100. 
+        Setting all three intensity values to 0 turns the LED off.
+        Example: Robot.setTail(“all”,0,100,0)"""
+    
+        if not (port == 1 or 2 or 3 or 4 or 'all'):
+            Errors.printError("SyntaxError: invalid selection")
+
+        Errors.checkIfInvalidRGBValue(r,g,b)
+
+        tailvaluesRGB = [port,r,g,b]
+        
+        return tailvaluesRGB
+    
+    @staticmethod
+    def playNote(note,beats):
+        """Plays a note using the buzzer on the Robot.
+        The method requires an integer representing the note (32-135) and a number giving the number of beats (0-16). 
+        The number of beats can be a decimal number. One beat corresponds to one second.
+        Example: Robot.playNote(60,0.5)"""
+        
+        if not note in range(31,136):
+            Errors.printError("ValueError: note is not in range")
+        
+        if beats < 1 or beats > 16:
+            Errors.printError("ValueError: beat is not in range")
+
+        options = [note,beats]
+
+        return options
+        
+        
+    @staticmethod
     def getVals(robot):
         """returns name of specified robot"""
         id = 1  
@@ -109,6 +165,17 @@ getvals = Robot.getVals(robot.name) # get the robot's name and put it in a getva
 
 beaklight = robot.BeakLight(100,255,100) # change the beakLight color to blue=100, red=255, green=100, and put it into "beaklight"
 
-robot.setTurn('R', 360, 100)
+turn1 = robot.setTurn('R', 360, 100) # turn R
 
-print(beaklight) # print the beaklight color
+print(turn1)
+print(beaklight)
+
+robot.setMotors(50,50) # set motors L and R to 50% 
+
+tailLights = robot.setTail(1,100,200,100) # set port 1 to r=100, g=200, b=100
+
+print(tailLights)
+
+music = robot.playNote(132, 16) # play note 132 at beat 16
+
+print(music)
